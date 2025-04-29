f21434b5cdb751493c1c29d8f6bed01328650b59
conettoovando <cris.onetto01@gmail.com>
2025-04-28 22:12:24 -0400

    Actualización de api para ser utilizada como microservicio, no una aplicación idependiente

--- database/CreateDataBase.sql
+++ database/CreateDataBase.sql
@@ -6,7 +6,15 @@ CREATE TABLE IF NOT EXISTS USERS (
     id char(36) primary key default (uuid()),
     username varchar(30) not null unique,
     password varchar(255) not null,
-    role varchar(30),
+    role varchar(30)
 );
 
 SELECT * FROM users;
+drop table USERS;
+
+delete from USERS where username = "Cristian";
+
+use finance;
+select * from users;
+drop database finance;
+create database finance;
--- database/connection.py
+++ database/connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:@localhost:3306/auth"
+URL_DATABASE = "mysql+pymysql://root:1972@localhost:3306/auth"
 
 def get_db():
     db = SessionLocal()
